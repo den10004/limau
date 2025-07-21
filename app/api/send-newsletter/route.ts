628162e822dec1d83273ad7c4ef841b072393d5f
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-07-21 10:27:08 +0300

    сделана рассылка писем

--- app/api/send-newsletter/route.ts
+++ app/api/send-newsletter/route.ts
@@ -38,22 +38,22 @@ export async function POST(req: NextRequest) {
               <meta charset="UTF-8" />
               <title>${title}</title>
             </head>
-            <body style="margin:0; padding:0; font-family: Arial, sans-serif; background-color: #f7f7f7;">
-              <table width="100%" cellpadding="0" cellspacing="0" style="background-color:#f7f7f7; padding: 20px;">
+            <body style="margin:0; padding:0; font-family: "Roboto", "Arial", sans-serif; background-color: #f7f7f7;">
+              <table width="100%" cellpadding="0" cellspacing="0" style="background-color: var(--color-grayAO); padding: 20px;">
                 <tr>
                   <td align="center">
-                    <table width="600" cellpadding="0" cellspacing="0" style="background-color: #ffffff; padding: 20px; border-radius: 8px;">
+                    <table width="600" cellpadding="0" cellspacing="0" style="background-color: var(----color-white); padding: 20px; border-radius: 8px;">
                       <tr>
                         <td>
-                          <h1 style="font-size: 20px; color: #333333;">${title}</h1>
-                          <div style="font-size: 16px; line-height: 1.5; color: #555555;">
+                          <h1 style="font-size: 20px; color: var(--color-black);">${title}</h1>
+                          <div style="font-size: 16px; line-height: 1.5; color: var(--color-black)">
                             ${htmlContent}
                           </div>
                         </td>
                       </tr>
                       <tr>
-                        <td style="padding-top: 20px; font-size: 12px; color: #999999; text-align: center;">
-                          © ${new Date().getFullYear()} Твой Блог. Все права защищены.
+                        <td style="padding-top: 20px; font-size: 12px; color: (var--color-gray56); text-align: center;">
+                          © ${new Date().getFullYear()} Limaudio. Все права защищены.
                         </td>
                       </tr>
                     </table>
