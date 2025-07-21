1e5620a2d3ea619d8e74e44204647602da0ca5fe
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-07-21 11:06:01 +0300

    форматирование рассылки писем

--- lib/emailTemplate.tsx
+++ lib/emailTemplate.tsx
@@ -7,22 +7,22 @@ export function emailTemplate(title: string, htmlContent: string) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>${title}</title>
       </head>
-      <body style="margin:0; padding:0; font-family: Arial, sans-serif; background-color: #f7f7f7;">
-        <table width="100%" cellpadding="0" cellspacing="0" style="background-color:#f7f7f7; padding: 20px;">
+      <body style="margin:0; padding:0; font-family: Arial, sans-serif; background-color: var(--color-white);">
+        <table width="100%" cellpadding="0" cellspacing="0" style="background-color: var(--color-white); padding: 20px;">
           <tr>
             <td align="center">
-              <table width="600" cellpadding="0" cellspacing="0" style="background-color: #ffffff; padding: 20px; border-radius: 8px;">
+              <table width="600" cellpadding="0" cellspacing="0" style="background-color: var(--color-white); padding: 20px; border-radius: 8px;">
                 <tr>
                   <td>
-                    <h1 style="font-size: 20px; color: #333333;">${title}</h1>
-                    <div style="font-size: 16px; line-height: 1.5; color: #555555;">
+                    <h1 style="font-size: 20px; color: var(--color-black);">${title}</h1>
+                    <div style="font-size: 16px; line-height: 1.5; color: var(--color-black);">
                       ${htmlContent}
                     </div>
                   </td>
                 </tr>
                 <tr>
-                  <td style="padding-top: 20px; font-size: 12px; color: #999999; text-align: center;">
-                    © ${new Date().getFullYear()} Limaudio. Все права защищены.
+                  <td style="padding-top: 20px; font-size: 12px; color: var(--color-grayAO); text-align: center;">
+                    © ${new Date().getFullYear()} <a href="https://limaudio.ru">Limaudio.</a> Все права защищены.
                   </td>
                 </tr>
               </table>
